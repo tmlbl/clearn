f4c6e6bd708d4b3f2f966abe3e77a6bbf6d52996
Tim Lebel <tim.lebel@gmail.com>
2025-09-03 11:11:43 -0400

    Failing test

--- bplus/bplus_test.c
+++ bplus/bplus_test.c
@@ -33,22 +33,22 @@ void test_set_and_get() {
     }
 }
 
-void test_split_and_save() {
+int test_split_and_save(int key_size, int num_keys) {
     char *filename = "/tmp/bplus_split_and_save";
 
     srand(time(NULL));
     struct bplus_tree *tree = bplus_tree_create(filename);
 
-    char key_buf[16];
-    char val_buf[16];
+    char *key_buf = malloc(key_size * num_keys);
+    char *val_buf = malloc(key_size * num_keys);
 
-    for (int i = 0; i < (MAX_KEYS * 2); i++) {
-        fill_random(&key_buf[0], 16);
-        fill_random(&val_buf[0], 16);
+    fill_random(key_buf, key_size * num_keys);
+    fill_random(val_buf, key_size * num_keys);
 
-        printf("insertion #%d\n", i);
-
-        bplus_tree_insert(tree, &key_buf[0], &val_buf[0]);
+    for (int i = 0; i < num_keys; i++) {
+        key_buf[(i * key_size) + key_size - 1] = '\0';
+        val_buf[(i * key_size) + key_size - 1] = '\0';
+        bplus_tree_insert(tree, &key_buf[i * key_size], &val_buf[i * key_size]);
     }
 
     bplus_tree_print_keys(tree);
@@ -61,12 +61,44 @@ void test_split_and_save() {
 
     bplus_tree_print_keys(tree);
 
+    // verify keys are in order
+    char *last_key = NULL;
+    char *this_key = malloc(16);
+
+    int ret = 0;
+
+    for (int i = 0; i <= tree->root->disk.num_keys; i++) {
+        struct bplus_node *node =
+            bplus_node_get_child(tree->pool, tree->root, i);
+        for (int j = 0; j < node->disk.num_keys; j++) {
+            memcpy(this_key, &node->disk.buf[node->disk.key_offsets[j]], 15);
+            this_key[15] = '\0';
+
+            if (last_key != NULL && strcmp(last_key, this_key) > 0) {
+                printf("keys out of order! %s > %s\n", last_key, this_key);
+                ret = 1;
+            }
+
+            if (last_key == NULL) {
+                last_key = malloc(16);
+            }
+
+            strcpy(last_key, this_key);
+        }
+    }
+
     bplus_tree_destroy(tree);
     remove(filename);
+
+    return ret;
 }
 
 int main(int argc, char *argv[]) {
+    int ret = 0;
     // test_set_and_get();
-    test_split_and_save();
-    return 0;
+    ret = test_split_and_save(16, 16);
+    if (ret != 0) {
+        return ret;
+    }
+    return ret;
 }
